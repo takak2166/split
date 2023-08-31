8178b331d4931858bbcee5395a87ddde5fe4dc09
takak2166 <28010438+takak2166@users.noreply.github.com>
2023-08-31 21:33:33 +0900

    commit for web test

---
+++ main.go
@@ -0,0 +1,340 @@
+package main
+
+import (
+	"bufio"
+	"crypto/md5"
+	"flag"
+	"fmt"
+	"io"
+	"math"
+	"os"
+	"regexp"
+	"strconv"
+)
+
+func parseByteSize(input string) (uint64, error) {
+	re := regexp.MustCompile(`^(\d+)([KMGTPkm]i?B?)?$`)
+	matches := re.FindStringSubmatch(input)
+	if len(matches) != 3 {
+		return 0, fmt.Errorf("Invalid byte size format: %s", input)
+	}
+
+	size, err := strconv.ParseUint(matches[1], 10, 64)
+	if err != nil {
+		return 0, err
+	}
+
+	unitVal := uint64(1)
+
+	unit := matches[2]
+	switch unit {
+	case "KB", "kB":
+		unitVal *= 1000
+	case "MB", "mB":
+		unitVal *= 1000 * 1000
+	case "GB":
+		unitVal *= 1000 * 1000 * 1000
+	case "TB":
+		unitVal *= 1000 * 1000 * 1000 * 1000
+	case "PB":
+		unitVal *= 1000 * 1000 * 1000 * 1000 * 1000
+	case "K", "k", "KiB", "kiB":
+		unitVal *= 1024
+	case "M", "m", "MiB", "miB":
+		unitVal *= 1024 * 1024
+	case "G", "GiB":
+		unitVal *= 1024 * 1024 * 1024
+	case "T", "TiB":
+		unitVal *= 1024 * 1024 * 1024 * 1024
+	case "P", "PiB":
+		unitVal *= 1024 * 1024 * 1024 * 1024 * 1024
+	case "Ki", "Mi", "Gi", "Ti", "Pi", "ki", "mi":
+		return 0, fmt.Errorf("Invalid byte size format %s", input)
+	}
+
+	result := size * unitVal
+
+	if result/unitVal != size {
+		return 0, fmt.Errorf("Parse is failed because of overflow")
+	}
+
+	return result, nil
+}
+
+type SplitType int
+
+const (
+	ByBytes SplitType = iota
+	ByLines
+	ByFiles
+)
+
+type Splitter struct {
+	splitType    SplitType
+	count        uint64
+	inputFile    *os.File
+	outputPrefix string
+}
+
+func NewSplitter(splitType SplitType, count uint64, inputFile *os.File, outputPrefix string) *Splitter {
+	return &Splitter{
+		splitType:    splitType,
+		count:        count,
+		inputFile:    inputFile,
+		outputPrefix: outputPrefix,
+	}
+}
+
+func (s *Splitter) Split() error {
+	switch s.splitType {
+	case ByBytes:
+		return s.splitByByte()
+	case ByLines:
+		return s.splitByLine()
+	case ByFiles:
+		return s.splitByFile()
+	}
+
+	return fmt.Errorf("invalid split type")
+}
+
+func (s *Splitter) splitByByte() error {
+	buffer := make([]byte, s.count)
+	for i := uint64(0); ; i++ {
+		n, err := s.inputFile.Read(buffer)
+		if err != nil {
+			if err == io.EOF {
+				break
+			} else {
+				return err
+			}
+		}
+
+		outputFile, err := s.createOutputFile(i)
+		defer outputFile.Close()
+		if err != nil {
+			return err
+		}
+
+		_, err = outputFile.Write(buffer[:n])
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (s *Splitter) splitByLine() error {
+	fileIndex := uint64(0)
+	lineCount := uint64(0)
+	outputFile, err := s.createOutputFile(uint64(fileIndex))
+	defer outputFile.Close()
+	if err != nil {
+		return err
+	}
+
+	buffer := bufio.NewReader(s.inputFile)
+	for i := 0; ; i++ {
+		line, err := buffer.ReadBytes('\n')
+		if err != nil {
+			if err == io.EOF {
+				_, err = outputFile.Write(line)
+				break
+			} else {
+				return err
+			}
+		}
+
+		_, err = outputFile.Write(line)
+		if err != nil {
+			return err
+		}
+		lineCount++
+
+		if lineCount%s.count == 0 {
+			outputFile.Close()
+			fileIndex++
+			outputFile, err = s.createOutputFile(fileIndex)
+			defer outputFile.Close()
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func (s *Splitter) splitByFile() error {
+	fileStat, err := s.inputFile.Stat()
+	if err != nil {
+		return err
+	}
+	fileSize := uint64(fileStat.Size())
+	byteCount := fileSize / s.count
+	byteRemain := fileSize % s.count
+	buffer := make([]byte, byteCount)
+	for i := uint64(0); i < s.count; i++ {
+		if i == s.count-1 {
+			buffer = make([]byte, byteCount+byteRemain)
+		}
+		n, err := s.inputFile.Read(buffer)
+		if err != nil {
+			if err == io.EOF {
+				break
+			} else {
+				return err
+			}
+		}
+
+		outputFile, err := s.createOutputFile(i)
+		defer outputFile.Close()
+		if err != nil {
+			return err
+		}
+
+		_, err = outputFile.Write(buffer[:n])
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (s *Splitter) createOutputFile(index uint64) (outputFile *os.File, err error) {
+	outputFileName, err := genFileName(s.outputPrefix, index, s.count, s.splitType)
+	if err != nil {
+		return nil, err
+	}
+
+	outputFile, err = os.Create(outputFileName)
+	if err != nil {
+		return outputFile, err
+	}
+	return outputFile, nil
+}
+
+// 生成されるファイル名の命名規則
+func genFileName(prefix string, index uint64, fileCount uint64, splitType SplitType) (string, error) {
+	if splitType == ByFiles && index+1 > fileCount {
+		return "", fmt.Errorf("invalid index")
+	}
+	if prefix == "" {
+		prefix = "x"
+	}
+	if splitType != ByFiles {
+		fileCount = 26
+		for i := 1; i < 14; i++ {
+			if uint64(math.Pow(26, float64(i)))-26 <= index && index < uint64(math.Pow(26, float64(i+1)))-26 {
+				index -= uint64(math.Pow(26, float64(i))) - 26
+				fileCount = uint64(math.Pow(26, float64(i))) + 1
+				for j := 1; j < i; j++ {
+					prefix += "z"
+				}
+				break
+			}
+		}
+	}
+	tmp := ""
+	if fileCount < 27 {
+		tmp = fmt.Sprintf("%c", 'a'+(index%26))
+		index /= 26
+	}
+	fileCountF := float64(fileCount)
+	for fileCountF >= 1 {
+		tmp = fmt.Sprintf("%c", 'a'+(index%26)) + tmp
+		if fileCountF/26 == 1 {
+			break
+		}
+		fileCountF /= 26
+		index /= 26
+	}
+	return prefix + tmp, nil
+}
+
+func main() {
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage: %s [options...] <file> [prefix]\n", os.Args[0])
+		flag.PrintDefaults()
+	}
+	// コマンドライン引数
+	byteCountStr := flag.String("b", "0", "Bytes per output file")
+	lineCountP := flag.Uint64("l", 0, "Number of lines per output file")
+	fileCountP := flag.Uint64("n", 0, "Number of output files")
+
+	flag.Parse()
+
+	byteCount, err := parseByteSize(*byteCountStr)
+	if err != nil {
+		panic(err)
+	}
+	lineCount := *lineCountP
+	fileCount := *fileCountP
+
+	// 複数の分割方法は指定不可
+	if byteCount > 0 && (lineCount > 0 || fileCount > 0) {
+		panic("You must specify only one option")
+	} else if lineCount > 0 && fileCount > 0 {
+		panic("You must specify only one option")
+	}
+
+	if fileCount > 0 && flag.Arg(0) == "" {
+		panic("You must specify file")
+	}
+
+	inputFilePath := flag.Arg(0)
+	outputPrefix := flag.Arg(1)
+
+	inputFile, err := os.Open(inputFilePath)
+	defer inputFile.Close()
+	if err != nil {
+		panic(err)
+	}
+
+	splitter := NewSplitter(ByLines, 1000, inputFile, outputPrefix)
+
+	if byteCount > 0 {
+		splitter = NewSplitter(ByBytes, byteCount, inputFile, outputPrefix)
+
+	} else if lineCount > 0 {
+		splitter = NewSplitter(ByLines, lineCount, inputFile, outputPrefix)
+
+	} else if fileCount > 0 {
+		splitter = NewSplitter(ByFiles, fileCount, inputFile, outputPrefix)
+	}
+
+	err = splitter.Split()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func getFileHash(filePath string) (string, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+
+	hash := md5.New()
+	_, err = io.Copy(hash, file)
+	if err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("%x", hash.Sum(nil)), nil
+}
+
+func compareFileHashes(filePath1, filePath2 string) (bool, error) {
+	hash1, err := getFileHash(filePath1)
+	if err != nil {
+		return false, err
+	}
+
+	hash2, err := getFileHash(filePath2)
+	if err != nil {
+		return false, err
+	}
+
+	return hash1 == hash2, nil
+}
---
+++ main_test.go
@@ -0,0 +1,173 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"testing"
+)
+
+func TestParseByteSize(t *testing.T) {
+	tests := []struct {
+		valid    bool
+		input    string
+		expected uint64
+	}{
+		{true, "1KB", 1000},
+		{true, "1kB", 1000},
+		{true, "20MB", 20 * 1000 * 1000},
+		{true, "20mB", 20 * 1000 * 1000},
+		{true, "300GB", 300 * 1000 * 1000 * 1000},
+		{true, "4000TB", 4000 * 1000 * 1000 * 1000 * 1000},
+		{true, "16383PB", 16383 * 1000 * 1000 * 1000 * 1000 * 1000},
+		{true, "1K", 1024},
+		{true, "1k", 1024},
+		{true, "1KiB", 1024},
+		{true, "1kiB", 1024},
+		{true, "20M", 20 * 1024 * 1024},
+		{true, "20MiB", 20 * 1024 * 1024},
+		{true, "20miB", 20 * 1024 * 1024},
+		{true, "300G", 300 * 1024 * 1024 * 1024},
+		{true, "4000T", 4000 * 1024 * 1024 * 1024 * 1024},
+		{true, "16383P", 16383 * 1024 * 1024 * 1024 * 1024 * 1024},
+		{true, "12345", 12345},
+		{false, "16384P", 0},
+		{false, "KB", 0},
+		{false, "K", 0},
+		{false, "Ki", 0},
+		{false, "ki", 0},
+		{false, "10B", 0},
+		{false, "10KA", 0},
+		{false, "10Ki", 0},
+		{false, "10ki", 0},
+		{false, "10g", 0},
+		{false, "10giB", 0},
+		{false, "10gB", 0},
+	}
+
+	for _, test := range tests {
+		t.Run(test.input, func(t *testing.T) {
+			result, err := parseByteSize(test.input)
+			if err != nil && test.valid {
+				t.Errorf("Error parsing byte size for input %s: %v", test.input, err)
+			}
+			if err == nil && !test.valid {
+				t.Errorf("Unexpected result : error is nil, result is %v", result)
+			}
+			if result != test.expected && test.valid {
+				t.Errorf("Unexpected result for input %s: got %d, expected %d", test.input, result, test.expected)
+			}
+		})
+	}
+}
+
+func TestGenFileName(t *testing.T) {
+	tests := []struct {
+		valid     bool
+		prefix    string
+		index     uint64
+		fileCount uint64
+		splitType SplitType
+		expected  string
+	}{
+		{true, "prefix", 0, 1, ByFiles, "prefixaa"},
+		{true, "prefix", 1, 2, ByFiles, "prefixab"},
+		{true, "", 0, 26, ByFiles, "xaa"},
+		{true, "", 0, 676, ByFiles, "xaa"},
+		{true, "", 25, 27, ByFiles, "xaz"},
+		{true, "", 26, 27, ByFiles, "xba"},
+		{true, "", 675, 676, ByFiles, "xzz"},
+		{true, "", 0, 677, ByFiles, "xaaa"},
+		{true, "", 676, 677, ByFiles, "xbaa"},
+		{true, "", 17575, 17576, ByFiles, "xzzz"},
+		{true, "", 0, 17577, ByFiles, "xaaaa"},
+		{true, "", 17576, 17577, ByFiles, "xbaaa"},
+		{false, "", 0, 0, ByFiles, ""},
+		{false, "", 1, 0, ByFiles, ""},
+		{true, "prefix", 0, 0, ByBytes, "prefixaa"},
+		{true, "", 1, 0, ByBytes, "xab"},
+		{true, "", 649, 0, ByBytes, "xyz"},
+		{true, "", 650, 0, ByBytes, "xzaaa"},
+		{true, "", 651, 0, ByBytes, "xzaab"},
+		{true, "", 676, 0, ByBytes, "xzaba"},
+		{true, "", 17549, 0, ByBytes, "xzyzz"},
+		{true, "", 17550, 0, ByBytes, "xzzaaaa"},
+		{true, "", 456949, 0, ByBytes, "xzzyzzz"},
+	}
+
+	for _, test := range tests {
+		t.Run(fmt.Sprintf("splitType: %v index: %d fileCount: %d", test.splitType, test.index, test.fileCount), func(t *testing.T) {
+			fileName, err := genFileName(test.prefix, test.index, test.fileCount, test.splitType)
+			if err != nil && test.valid {
+				t.Errorf("Unexpected error: %s", err)
+			}
+			if fileName != test.expected && test.valid {
+				t.Errorf("Unexpected result: got %s, expected %s", fileName, test.expected)
+			}
+		})
+	}
+}
+
+func TestSplitterSplit(t *testing.T) {
+	wd, _ := os.Getwd()
+	wd += "/"
+	inputTestFileDir := wd + "testfiles/input/"
+	outputTestFileDir := wd + "testfiles/output/"
+	tests := []struct {
+		name                string
+		splitType           SplitType
+		count               uint64
+		inputFileName       string
+		outputFilePrefix    string
+		expectedOutputNames []string
+	}{
+		{
+			name:                "split by bytes",
+			splitType:           ByBytes,
+			count:               50,
+			inputFileName:       "sample.txt",
+			outputFilePrefix:    "bytes_",
+			expectedOutputNames: []string{"bytes_aa", "bytes_ab", "bytes_ac", "bytes_ad", "bytes_ae", "bytes_af", "bytes_ag", "bytes_ah"},
+		},
+		{
+			name:                "split by lines",
+			splitType:           ByLines,
+			count:               2,
+			inputFileName:       "sample.txt",
+			outputFilePrefix:    "lines_",
+			expectedOutputNames: []string{"lines_aa", "lines_ab", "lines_ac"},
+		},
+		{
+			name:                "split by files",
+			splitType:           ByFiles,
+			count:               3,
+			inputFileName:       "sample.txt",
+			outputFilePrefix:    "files_",
+			expectedOutputNames: []string{"files_aa", "files_ab", "files_ac"},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			inputFile, _ := os.Open(inputTestFileDir + test.inputFileName)
+			defer inputFile.Close()
+
+			splitter := NewSplitter(test.splitType, test.count, inputFile, test.outputFilePrefix)
+			splitter.Split()
+
+			for _, expectedOutputName := range test.expectedOutputNames {
+				expectedOutputFilePath := outputTestFileDir + expectedOutputName
+				outputFilePath := wd + expectedOutputName
+				defer os.Remove(outputFilePath)
+
+				ok, err := compareFileHashes(expectedOutputFilePath, outputFilePath)
+				if err != nil {
+					t.Errorf("Unexpected error: %s", err)
+					return
+				}
+				if !ok {
+					t.Errorf("The result of comparing %v and %v is not as expected.", expectedOutputFilePath, outputFilePath)
+					return
+				}
+			}
+		})
+	}
+}
